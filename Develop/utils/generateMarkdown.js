a05a7d0d2d30545fff4cc58a99cb74d3620040ec
cpayment <cpayment12@gmail.com>
2024-01-10 13:09:19 -0600

    Finished adding renderLicenseLink function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -19,7 +19,20 @@ function renderLicenseLink(license) {
     case 'MIT':
       licenseLink = 'https://mit-license.org/'
       break;
+    case 'BSD':
+      licenseLink = 'https://opensource.org/licenses/BSD-3-Clause';
+      break;
+    case 'GPL':
+      licenseLink = 'https://www.gnu.org/licenses/gpl-3.0.en.html';
+      break;
+    case 'Apache':
+      licenseLink = 'https://www.apache.org/licenses/LICENSE-2.0.html';
+      break;
+    default:
+      licenseLink = '';
+      break;
   }
+  return licenseLink;
 }
 
 // TODO: Create a function that returns the license section of README
