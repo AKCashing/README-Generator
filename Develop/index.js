81a7cd0428130641b306638c91eb54f9ca816b1e
cpayment <cpayment12@gmail.com>
2024-01-10 12:38:53 -0600

    finished renderLicenseBadge and created array for questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
+// npm i inquirer@8.2.4
+// npm install prompt-sync
+const prompt = require('prompt-sync')({sigint:true});
 
 // TODO: Create an array of questions for user input
-const questions = [];
+let q1 = prompt('Type here: ');
+const questions = [q1];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = '';
+
+  if(license != 'None') {
+    badge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
